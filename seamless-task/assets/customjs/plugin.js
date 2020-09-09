ab4d3f60cb117c5593e11ca5c825e9baf8b5eed0
Zeinab M. Aamer <zeinabaamer157@gmail.com>
2020-09-09 12:26:32 +0200

    Auto-generated commit

--- seamless-task/assets/customjs/plugin.js
+++ seamless-task/assets/customjs/plugin.js
@@ -1,17 +1,3 @@
-
-$(function(){
-    $(document).on("click", 'a[href^="#"]', function(e) {
-        e.preventDefault();
-    
-        $("html, body")
-          .stop()
-          .animate(
-            {
-              scrollTop: $($(this).attr("href")).offset().top
-            },
-    
-            1000,
-            "linear"
-          );
-    });
-})
+function scrollToElement(id){
+   document.getElementById(id + '').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});   
+}
